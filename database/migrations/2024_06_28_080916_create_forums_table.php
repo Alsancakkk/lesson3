1cb050244d90780e4a9ac778b9145122eda0ef33
Alsancakkk <canalsancak99@gmail.com>
2024-07-09 11:06:42 +0300

    edit table

--- database/migrations/2024_06_28_080916_create_forums_table.php
+++ database/migrations/2024_06_28_080916_create_forums_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('description');
-            $table->foreignId('category_name')->constrained()->onDelete('cascade');
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
